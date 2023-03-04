8c9f8d2bb28b93f959ba21efa9289c4d04ab751f
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2023-03-04 11:16:09 +0200

    add redux

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-// import { useState } from "react";
+import { Provider } from "react-redux";
 import { useRoute } from "./router";
+import { store } from "./redux/store";
 
 export default function App() {
-  const content = useRoute(true);
-  return <NavigationContainer>{content}</NavigationContainer>;
+  const content = useRoute(false);
+  return (
+    <Provider store={store}>
+      <NavigationContainer>{content}</NavigationContainer>
+    </Provider>
+  );
 }
